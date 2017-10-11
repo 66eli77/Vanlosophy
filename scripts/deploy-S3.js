0090531b9d71e6a5f52f02d22f5da66a8ac49d7f
Eli Dai <66eli77@gmail.com>
2017-10-10 18:39:21 -0700

    routing rules 50 limit work around.

--- scripts/deploy-S3.js
+++ scripts/deploy-S3.js
@@ -8,7 +8,7 @@
 
 const fs = require('fs'),
   keys = JSON.parse(fs.readFileSync('secret/rootkey.json', 'utf8')),
-  redirects = JSON.parse(fs.readFileSync('config/redirects.json', 'utf8')),
+  redirects302 = JSON.parse(fs.readFileSync('config/redirects.json', 'utf8')),
   maxAge = 'max-age=1209600', // 14 days.
   bucketName = 'vanlosophy.com';
 
@@ -31,8 +31,25 @@ client = s3.createClient({
   },
 });
 
+//try 200 redirects, and I hit the 50 routing rules limit :(
+/*
+var twoHundredsRedirects = [];
+for(var i = 0; i < 200; i++){
+  moreDirects.push({
+     "Redirect": {
+      "HostName": "vanlosophy.com",
+      "HttpRedirectCode": "302",
+      "ReplaceKeyWith": ""
+    },
+    "Condition": {
+      "KeyPrefixEquals": "page" + i
+    }
+  })
+}
+*/
 
 // update the WebsiteConfiguration.
+// Noticed! WebsiteConfiguration has a 50 routing rules limit, so we only use it for 302 redirects.
 routingRulesParams = {
   Bucket: bucketName, 
   ContentMD5: "", 
@@ -43,15 +60,15 @@ routingRulesParams = {
     IndexDocument: {
       Suffix: "index.html"
     },
-    RoutingRules: redirects
+    RoutingRules: redirects302
   }
 };
 promises.push(new Promise((resolve, reject) => {
-  console.log('Updating routing rules...');
+  console.log('Updating 302 routing rules...');
   client.s3.putBucketWebsite(routingRulesParams, function(err, data) {
     if (err) console.log(err, err.stack); // an error occurred
     else     console.log(data);           // successful response
-    console.log('Routing rules update finished.');
+    console.log('302 Routing rules update finished.');
     resolve();
   });
 }));
@@ -103,8 +120,43 @@ promises.push(new Promise((resolve, reject) => {
 
 // update for both routing rules and object syncing is done.
 Promise.all(promises).then( v => {
-  console.log('All taskes completed!');
-  process.exit();
+  // work around of the 50 routing rules limit
+  // it has to take place after dir sync is done, otherwise this will be overwrited
+  var promises301 = [];
+  promises301.push(new Promise((resolve, reject) => {
+    var dummyParams = {
+      localFile: "config/dummy.html",
+     
+      s3Params: {
+        Bucket: bucketName,
+        Key: "limit50.html",
+        WebsiteRedirectLocation: "/features"
+        // other options supported by putObject, except Body and ContentLength. 
+        // See: http://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/S3.html#putObject-property 
+      },
+    };
+    var uploader = client.uploadFile(dummyParams);
+    uploader.on('error', function(err) {
+      console.error("unable to upload:", err.stack);
+      reject();
+    });
+    uploader.on('progress', function() {
+      console.log("progress", uploader.progressMd5Amount,
+                uploader.progressAmount, uploader.progressTotal);
+    });
+    uploader.on('end', function() {
+      console.log("301 redirect done uploading");
+      resolve();
+    });
+  }));
+
+  Promise.all(promises301).then( v => {
+    console.log('All taskes completed!');
+    process.exit();
+  }).catch(reason => { 
+    console.log(reason);
+    process.exit();
+  });
 }).catch(reason => { 
   console.log(reason);
   process.exit();
